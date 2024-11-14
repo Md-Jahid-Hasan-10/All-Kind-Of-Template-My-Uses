8d5bb359d16eaea1304e3cf59b6fdf97255ec2ca
Mohammad Jahid Hasan <122695080+Md-Jahid-Hasan-10@users.noreply.github.com>
2024-11-14 23:07:52 +0600

    update

--- Range_intersection_binary_search.cpp
+++ Range_intersection_binary_search.cpp
@@ -29,7 +29,7 @@ typedef vector<pii> vii;
 typedef vector<pll> vll;
 typedef long double dl;
 
-const ll INF = 1e9+12;
+const ll INF = 1e10;
 const ll Mod = 1e9+7;
 const ll N = 2e5+12;
 
@@ -37,38 +37,35 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<dl> x(n),v(n);
-    for(int i = 0; i < n ; i++){
-        cin >> x[i];
-    }
+    vii ar;
     for(int i = 0; i < n; i++){
-        cin >> v[i];
+        int a,b;
+        cin >> a >> b;
+        ar.push_back({a,b});
     }
-    auto f = [&] (dl m) -> bool{
+    auto f = [&] (dl time) -> bool
+    {
         vector<pair<dl,dl>> p;
-        for(int i = 0; i < n; i++){
-            dl r = x[i] + m*v[i];
-            dl l = x[i] - m*v[i];
+        for(auto u : ar){
+            dl l = u.F - (time*u.S);
+            dl r = u.F + (time*u.S);
             p.push_back({l,r});
         }
-        dl l = -INF,r = INF;
-        for(int i = 0; i < n; i++){
-            dl left = p[i].F;
-            dl right = p[i].S;
-            if(l > right || r < left){
-                return 0;
-            }
-            l = max(l,left);
-            r = min(r,right);
+        dl left = - 1e10,right = 1e10;
+        for(auto u : p){
+            dl l = u.F,r = u.S;
+            if(r < left || l > right)return 0;
+            left = max(left,l);
+            right = min(right,r);
         }
         return 1;
     };
-    dl lo = 0,hi = INF,ans = INF;
+    dl lo = 0, hi = 1e9+2,ans = 1e10,mid;
     for(int i = 0; i < 100; i++){
-        dl mid = (lo + hi)/2;
+        mid = (lo + hi)/2;
         if(f(mid)){
-            ans = mid;
             hi = mid;
+            ans = min(ans,mid);
         }
         else lo = mid;
     }
