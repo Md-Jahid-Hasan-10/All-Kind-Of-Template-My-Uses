3d3a2a94c07b10dbabfa7186a0b09cdc35397e09
Mohammad Jahid Hasan <122695080+Md-Jahid-Hasan-10@users.noreply.github.com>
2024-11-26 16:22:21 +0600

    Update Binary-Index-Tree.cpp

--- Binary-Index-Tree.cpp
+++ Binary-Index-Tree.cpp
@@ -11,6 +11,7 @@ using namespace std;
     ios_base::sync_with_stdio(0);  \
     cin.tie(0);                    \
     cout.tie(0);
+const int N = 2e6 + 7;
 struct Bit {
     long long BIT[N];
     void update(int x, int delta) {
